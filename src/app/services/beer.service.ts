16d55f4aa37a791086c03e38e8c55c30bc569a84
Norinot <naruto010190@gmail.com>
2022-10-25 13:14:13 +0200

    cart update, wishlist update, details component

--- src/app/services/beer.service.ts
+++ src/app/services/beer.service.ts
@@ -2,14 +2,43 @@ import { Injectable } from '@angular/core';
 import { Beer } from '../interfaces/beer';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BeerService {
-
   Beers: Beer[] = [];
-
+  carouselBeers: Beer[] = [];
+  filteredBeers: Beer[] = [];
+  wishListedBeers: Beer[] = [];
+  beerCount: number[] = [];
+  navigatedToCart: boolean = false;
+  subTotal: number[] = [];
   inCart: Beer[] = [];
 
   selectedBeer?: Beer;
-  constructor() { }
+  constructor() {}
+
+  putToWishList(beer: Beer): boolean {
+    if (this.wishListedBeers.includes(beer)) {
+      this.wishListedBeers =
+        this.wishListedBeers.filter((item) => item !== beer);
+      return false;
+    } else {
+      this.wishListedBeers.push(beer);
+      return true;
+    }
+  }
+
+  cartCount(){
+    let uniqueBeers = [...new Set(this.inCart)];
+
+    this.beerCount = uniqueBeers.map((beer) => {
+      return this.inCart.filter((item) => item === beer).length;
+    });
+
+    this.subTotal = uniqueBeers.map((beer) => {
+      return this.inCart.filter((item) => item === beer).length * beer.ibu;
+    });
+
+    return uniqueBeers;
+  }
 }
